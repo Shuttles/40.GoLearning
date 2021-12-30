65e3fd4b5f607a421a572efb9234ad3c6eb6709a
chenzheyu <2287441467@qq.com>
2021-12-30 20:29:37 +0800

    some changres

--- GinkgoExample/books/book_test.go
+++ GinkgoExample/books/book_test.go
@@ -42,6 +42,7 @@ var _ = Describe("Book", func() {
 	Describe("Categorizing book length", func() {
 		Context("With more than 300 pages", func() {
 			It("should be a novel", func() {
+				Expect(longBook.CategoryByLength()).To(Equal("NOVEL!"))
 				Expect(longBook.CategoryByLength()).To(Equal("NOVEL"))
 			})
 		})
